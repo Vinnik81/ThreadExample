9899be39e24251bf85dbecf6cf4f29c3aa7d3170
ВинБоцы ВинБоцы <solarisraver@mail.ru>
2022-11-13 00:27:56 +0600

    Add project files.

---
+++ HomeWork_EggOrChicken/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace HomeWork_EggOrChicken
+{
+    class Program
+    {
+        static Thread chickenVoice;
+        static Thread eggVoice;
+        static int count = 0;
+        static void Main(string[] args)
+        {
+            chickenVoice = new Thread(new ThreadStart(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Thread.Sleep(new Random().Next(100, 500));
+                    Console.WriteLine(count++ + ") Chicken # " + i);
+                }
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("+++++++++++++++++++++++++++++++Конец потока Chicken+++++++++++++++++++++++++++++++++++++++++");
+                Console.ResetColor();
+                if (eggVoice.ThreadState == ThreadState.Stopped) Console.WriteLine("Первым было яйцо");
+            }));
+
+            eggVoice = new Thread(new ThreadStart(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Thread.Sleep(new Random().Next(100, 500));
+                    Console.WriteLine(count++ + ") Egg! # " + i);
+                }
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("+++++++++++++++++++++++++++++++Конец потока Egg+++++++++++++++++++++++++++++++++++++++++++++");
+                Console.ResetColor();
+                if (chickenVoice.ThreadState == ThreadState.Stopped) Console.WriteLine("Первым была курица");
+            }));
+            eggVoice.Start();
+            chickenVoice.Start();
+        }
+    }
+}
