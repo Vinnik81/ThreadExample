d1e3287e11ffc32df5220d93009ef95f4750e016
ВинБоцы ВинБоцы <solarisraver@mail.ru>
2022-11-20 18:48:40 +0600

    HW_Thread_Task1

---
+++ HomeWork_ThreadExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace HomeWork_ThreadExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первый элемент массива: ");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Введите последний элемент массива: ");
+            int count = Int32.Parse(Console.ReadLine());
+            List<int> list = new List<int>(count);
+            for (int i = a; i < count; i++) list.Add(i);
+            foreach (var item in list) Console.Write(item + " ");
+            Thread MaxVal = null;
+            Thread MinVal = null;
+            MaxVal = new Thread(new ThreadStart(() =>
+            {
+                var max = list.Max();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nМаксимальный элемент: " + max);
+               if(MinVal.ThreadState == ThreadState.Stopped) Console.ResetColor();
+            }));
+            MinVal = new Thread(new ThreadStart(() =>
+            {
+                var min = list.Min();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nМинимальный элемент: " + min);
+               if(MaxVal.ThreadState == ThreadState.Stopped) Console.ResetColor();
+            }));
+            MaxVal.Start();
+            MinVal.Start();
+        }
+    }
+}
